46bf38fce7f018c59f6eb4b761adc8258ab3989c
Alex42 <alexcrazy42@mail.ru>
2023-04-27 10:33:34 +0300

    first

---
+++ Patterns/GenerativePatterns/BuilderPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Pattern builder provides simple API for step-by-step contructing difficult object  
+*/
+
+Console.WriteLine("Привет");
+
+class BlogPost
+{
+    public string title;
+    public string text;
+    public List<string> tags;
+    public List<string> categories;
+
+    // we can make a constructor, but it not very comfortable for many class properties
+    // could to use pattern builder
+}
