61daa7517873ed5f43ee4a4eeb7943064e612e04
Alex42 <alexcrazy42@mail.ru>
2023-07-04 20:12:01 +0300

    add builder

---
+++ Patterns/GenerativePatterns/Builder/EmployeeReportBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class EmployeeReportBuilder : IEmployeeReportBuilder
+    {
+        private EmployeeReport _employeeReport;
+
+        private readonly IEnumerable<Employee> _employees;
+
+        public EmployeeReportBuilder(IEnumerable<Employee> employees)
+        {
+            _employees = employees;
+            _employeeReport = new EmployeeReport();
+        }
+
+        public void BuildHeader()
+        {
+            _employeeReport.Header =
+                $"Employee report on date: {DateTime.Now}\n";
+            _employeeReport.Header +=
+                "\n---------------------------------------\n";
+        }
+
+        public void BuildBody()
+        {
+            _employeeReport.Body =
+                String.Join(Environment.NewLine,
+                            _employees.Select(e =>
+                            $"Employee: {e.Name} \t\tSalary: {e.Salary}$"));
+        }
+
+        public void BuildFooter()
+        {
+            _employeeReport.Footer = "\n---------------------------------------";
+            _employeeReport.Footer +=
+                $"\nTotal employees: {_employees.Count()}, " +
+                $"Total salary: {_employees.Sum(e => e.Salary)}";
+        }
+
+        public EmployeeReport GetReport()
+        {
+            EmployeeReport employeeReport = _employeeReport;
+            _employeeReport = new();
+            return employeeReport;
+        }
+    }
+}
---
+++ Patterns/GenerativePatterns/Builder/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Builder;
+
+List<Employee> employees = new List<Employee>()
+{
+    new Employee { Name  = "Ivan", Salary = 100},
+    new Employee { Name  = "Boris", Salary = 500},
+    new Employee { Name  = "Alex", Salary = 200}
+
+};
+
+var builder = new EmployeeReportBuilder(employees);
+
+var director = new EmployeeReportDirector(builder);
+
+director.Build();
+
+var report = builder.GetReport();
+
+Console.WriteLine(report);
